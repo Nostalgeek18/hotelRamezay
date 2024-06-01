74bc1d60f40f4e655e4fd6b3cfeb4b5b569b6772
Nazim Mouzai <choco18@live.ca>
2024-06-01 14:59:43 -0400

    push

--- formulaires/envoiEmail.php
+++ formulaires/envoiEmail.php
@@ -7,10 +7,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $langue = htmlspecialchars($_POST['langue']);
 
     // Prepare the email content
-    $to = $email; // Sending email to the user
+    $to = "mouzai.n@yahoo.com"; // Sending email to the user
     $subject = "Votre formulaire a été soumis";
     $message = "Bonjour $nom,\n\nMerci pour vos commentaires:\n\n$commentaires\n\nLangue: $langue\n\nCordialement,\nVotre équipe";
-    $headers = "From: noreply@yourdomain.com";
+    $headers = "From: choco18@live.ca";
 
     // Send the email
     if (mail($to, $subject, $message, $headers)) {
